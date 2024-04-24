a3fe2eb8586c46f7dcc3798975ccc1a87e3a89f4
vojtacerny11 <extycz11@gmail.com>
2024-04-24 19:06:42 +0200

    update added sounds and music

--- hra/main.cpp
+++ hra/main.cpp
@@ -7,7 +7,7 @@
 #define SCREENHEIGHT 750
 
 #define CellHeight 250
-#define CellWidth 245
+#define CellWidth 250
 
 unsigned AnimationDelay = 5;
 unsigned AnimationDelayCount = 0;
@@ -15,17 +15,36 @@ int playerIndex = 0;
 bool IsFacingRight = 1;
 
 Vector2 playerPos = { SCREENWIDTH / 5, SCREENHEIGHT - CellHeight};
+Vector2 opponentPos = { SCREENWIDTH, SCREENHEIGHT - CellHeight };
 
 Texture2D spritePlayer;
 Texture2D spriteOpponent;
 
 Rectangle source = (Rectangle{ 0, 0, 250, 250 });
 Rectangle sourceL = (Rectangle{ 0, 250, 250, 250 });
+
+void GAMEOVER()
+{
+	Music music2 = LoadMusicStream("audio/titanic.mp3");
+	PlayMusicStream(music2);
+	while (WindowShouldClose() == 0)
+	{
+		UpdateMusicStream(music2);
+		BeginDrawing();
+		DrawText("YOU LOST", SCREENWIDTH / 2 - 225 / 2, SCREENHEIGHT / 2 - 50, 50, BLACK);
+		ClearBackground(RAYWHITE);
+		EndDrawing();
+	}
+}
+
 int main()
 {
 	InitWindow(SCREENWIDTH, SCREENHEIGHT, "2DSHOOTER");
 	InitAudioDevice();
 	Music music = LoadMusicStream("audio/Night.mp3");
+	Sound step = LoadSound("audio/step.wav");
+	SetSoundVolume(step, 0.5f);
+	SetMusicVolume(music, 0.75f);
 	PlayMusicStream(music);
 	SetTargetFPS(60);
 	spritePlayer = LoadTexture("assets/person2.png");
@@ -45,8 +64,9 @@ int main()
 				sourceL.x = (float)CellWidth * (float)playerIndex;
 			}
 		BeginDrawing();
-		if (IsKeyPressed(KEY_D))
+		if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT))
 		{
+			PlaySound(step);
 			playerPos.x += CellWidth;
 			//if (playerPos.x < 0 || playerPos.y < 0 || playerPos.x > SCREENWIDTH || playerPos.y > SCREENHEIGHT - CellHeight)
 			if (playerPos.x > SCREENWIDTH - CellWidth)
@@ -55,8 +75,9 @@ int main()
 			}
 			IsFacingRight = 1;
 		}
-		if (IsKeyPressed(KEY_A))
+		if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT))
 		{
+			PlaySound(step);
 			playerPos.x -= CellWidth;
 			if (playerPos.x < 0)
 			{
@@ -64,16 +85,18 @@ int main()
 			}
 			IsFacingRight = 0;
 		}
-		if (IsKeyPressed(KEY_S))
+		if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN))
 		{
+			PlaySound(step);
 			playerPos.y += CellHeight;
 			if (playerPos.y > SCREENHEIGHT - CellHeight)
 			{
 				playerPos.y -= CellHeight;
 			}
 		}
-		if (IsKeyPressed(KEY_W))
+		if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP))
 		{
+			PlaySound(step);
 			playerPos.y -= CellHeight;
 			if (playerPos.y < 0)
 			{
@@ -85,9 +108,11 @@ int main()
 		DrawTextureRec(spritePlayer, source, playerPos, WHITE);
 		if (!IsFacingRight)
 		DrawTextureRec(spritePlayer, sourceL, playerPos, WHITE);
+		if (playerPos.x == opponentPos.x && playerPos.y == opponentPos.y)
+			break;
 		EndDrawing();
 	}
-
+	GAMEOVER();
 
 	CloseAudioDevice();
 
